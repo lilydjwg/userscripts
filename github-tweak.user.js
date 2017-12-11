5963dd9813bbfff1b24a94f2ddbf938834feaba8
lilydjwg <lilydjwg@gmail.com>
2017-12-11 15:57:36 +0800

    github-tweak: change ZIP to GZIP after page changes

--- github-tweak.user.js
+++ github-tweak.user.js
@@ -3,19 +3,23 @@
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    use gzip, use ssh
 // @include        https://github.com/*
-// @version	   1.8.1
+// @version	   1.9
 // @grant          none
 // ==/UserScript==
  
 (function() {
 'use strict'
 
-const  dl = document.querySelector('a[data-ga-click*="download zip"]')
-if(dl){
-  dl.dataset.gaClick = dl.dataset.gaClick.replace('zip', 'gzip')
-  dl.href = dl.href.replace(/\.zip$/, '.tar.gz')
-  dl.textContent = dl.textContent.replace('ZIP', 'GZIP')
+function prefer_gzip() {
+  const dl = document.querySelector('a[data-ga-click*="download zip"]')
+  if(dl){
+    dl.dataset.gaClick = dl.dataset.gaClick.replace('zip', 'gzip')
+    dl.href = dl.href.replace(/\.zip$/, '.tar.gz')
+    dl.textContent = dl.textContent.replace('ZIP', 'GZIP')
+  }
 }
+
+prefer_gzip()
 
 const repourl = document.querySelectorAll('.js-live-clone-url')
 const re = /https:\/\/github\.com\/([^\/]+)\/(.*)/
@@ -28,5 +32,13 @@ for(i=0, len=repourl.length; i<len; i++){
     span.textContent = 'git@github.com:'+m[1]+'/'+m[2]
   }
 }
+
+document.addEventListener('click', function(e) {
+  if(e.target.tagName == 'SUMMARY' || (
+    e.target.tagName == 'SPAN' && e.target.classList.contains('dropdown-caret')
+  )) {
+    prefer_gzip()
+  }
+})
 
 })()
