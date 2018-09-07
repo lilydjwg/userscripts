f0287c0fe7a1578f5f4514a12e987a83e255b378
lilydjwg <lilydjwg@gmail.com>
2018-09-07 17:13:14 +0800

    mp.weixin: try to run multiple times

--- mp.weixin.user.js
+++ mp.weixin.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    weixin article fixes
 // @include        https://mp.weixin.qq.com/s*
-// @version	   0.1.1
+// @version	   0.1.2
 // @run-at	   document-idle
 // @grant          none
 // ==/UserScript==
@@ -16,6 +16,8 @@ const fix = function() {
 }
 
 setTimeout(fix, 1500)
+setTimeout(fix, 3000)
+setTimeout(fix, 5000)
 
 })()
 
