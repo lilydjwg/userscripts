1fbfc27bb08016bcea036667b996af5a9cb3584d
lilydjwg <lilydjwg@gmail.com>
2022-08-28 13:37:37 +0800

    bilibili: fix

--- bilibili.user.js
+++ bilibili.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    Bilibili improvements
 // @include        https://www.bilibili.com/video/*
-// @version        0.1.1
+// @version        0.1.2
 // @grant          none
 // ==/UserScript==
 
@@ -14,11 +14,10 @@ if(localStorage.recommend_auto_play == 'open') {
   document.querySelector('.next-button').click()
 }
 
-const settings = JSON.parse(localStorage.bilibili_player_settings)
-
-if(settings.video_status.autoplay) {
+const settings = JSON.parse(localStorage.bpx_player_profile)
+if(settings.media.autoplay) {
   settings.video_status.autoplay = false
-  localStorage.bilibili_player_settings = JSON.stringify(settings)
+  localStorage.bpx_player_profile = JSON.stringify(settings)
 }
 
 const v = document.querySelector('video')
@@ -29,27 +28,7 @@ if(v.muted) {
   v.currentTime = 0
 }
 
-const annoying = document.querySelector('.login-tip .close')
-if(annoying) {
-  annoying.click()
 }
-
-const annoying2 = document.querySelector('body > [role="tooltip"]')
-if(annoying2) {
-  annoying2.style.display = 'none'
-}
-
-const annoying3 = document.querySelector('.bilibili-player-video-toast-bottom > .bilibili-player-video-toast-item')
-if(annoying3) {
-  annoying3.style.display = 'none'
-}
-
-}
-
-const p = document.getElementById('bilibiliPlayer')
-p.addEventListener('dblclick', function(p) {
-  document.querySelector('.bilibili-player-video-btn-fullscreen > button').click()
-})
 
 setTimeout(doit, 1000)
 setTimeout(doit, 2000)
