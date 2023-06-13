a32c7ae013cc2ddd214435ed100f949de045a3fd
lilydjwg <lilydjwg@gmail.com>
2023-06-13 09:59:40 +0800

    youtube-cc-lang: fix again

--- youtube-cc-lang.user.js
+++ youtube-cc-lang.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    set YouTube subtitles lang attribute
 // @include        https://www.youtube.com/watch?*
-// @version	   0.3.1
+// @version	   0.3.2
 // @grant          none
 // ==/UserScript==
 
@@ -39,14 +39,16 @@ function start() {
   if(!button) {
     setTimeout(start, 1000)
   }
-  const el = document.querySelector('.ytp-popup.ytp-settings-menu .ytp-menuitem:nth-child(2) .ytp-menuitem-content')
-  if(!el && !btn_clicked) {
+  // const el = document.querySelector('.ytp-popup.ytp-settings-menu .ytp-menuitem:nth-child(2) .ytp-menuitem-content')
+
+  const r = document.evaluate('//div[@class="ytp-popup ytp-settings-menu"]//span[text()="字幕"]/parent::div/parent::div/parent::div/div[@class="ytp-menuitem-content"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null)
+  if(r.snapshotLength == 0 && !btn_clicked) {
     document.querySelector('.ytp-settings-button').click()
     btn_clicked = true
     setTimeout(start, 100)
   } else {
     document.querySelector('.ytp-settings-button').click()
-    run(el)
+    run(r.snapshotItem(0))
   }
 }
 
