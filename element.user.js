7964cda185045d3abf378216e18f158e8ce42df0
lilydjwg <lilydjwg@gmail.com>
2023-07-16 00:01:42 +0800

    element: bypass csp and update

--- element.user.js
+++ element.user.js
@@ -2,11 +2,12 @@
 // @name           Element.io tweak
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    Element.io tweak
-// @include        https://chat.mozilla.org/*
-// @include        https://app.element.io/*
-// @version        0.1
+// @match          https://chat.mozilla.org/*
+// @match          https://app.element.io/*
+// @version        0.1.1
 // @run-at         document-idle
 // @grant          none
+// @sandbox        JavaScript
 // ==/UserScript==
 
 const ICON_URL = 'https://telegram.org/img/website_icon.svg'
@@ -38,7 +39,7 @@ img_reply.style.left = '-6px'
 img_reply.style.marginRight = '-6px'
 
 function doit() {
-  for(let avatar of document.querySelectorAll('ol.mx_RoomView_MessageList img[title^="@telegram_"][title$=":t2bot.io"], ol.mx_RoomView_MessageList img[title^="@telegram_"][title$=":nichi.co"]')) {
+  for(let avatar of document.querySelectorAll('ol.mx_RoomView_MessageList img[title^="@telegram_"][title$=":t2bot.io"], ol.mx_RoomView_MessageList img[title^="@telegram_"][title$=":nichi.co"], ol.mx_RoomView_MessageList img[title^="@telegram_"][title$=":moe.cat"]')) {
     const width = avatar.width
     if(avatar.parentNode.tagName == 'SPAN' && width == 16) { // in reply to
       if(avatar.nextSibling.nodeType == Node.TEXT_NODE) {
