486cd77d0758ace3941f55c645343798f4ebbe58
lilydjwg <lilydjwg@gmail.com>
2023-11-17 17:37:34 +0800

    youtube-cc-lang: be more resilient

--- youtube-cc-lang.user.js
+++ youtube-cc-lang.user.js
@@ -3,8 +3,8 @@
 // @namespace      https://github.com/lilydjwg/userscripts
 // @description    set YouTube subtitles lang attribute
 // @match          https://www.youtube.com/watch?*
-// @version	   0.3.3
-// @grant          none
+// @version	   0.3.5
+// @grant          window.onurlchange
 // ==/UserScript==
 
 (function() {
@@ -12,8 +12,13 @@
 
 const ZH_TW_CHANNELS = ['PanSci 泛科学']
 
+let observer
+
 function run(el) {
-  const ob = new MutationObserver((mutationList, observer) => {
+  if(observer) {
+    observer.disconnect()
+  }
+  observer = new MutationObserver((mutationList, observer) => {
     mutationList.forEach((mutation) => {
       const name = mutation.target.textContent
       const cc = document.getElementById('ytp-caption-window-container')
@@ -37,7 +42,7 @@ function run(el) {
   })
 
   console.log('ytcc: observing')
-  ob.observe(el, { childList: true })
+  observer.observe(el, { childList: true })
 }
 
 let btn_clicked = false
@@ -51,17 +56,31 @@ function start() {
 
   const r = document.evaluate('//div[@class="ytp-popup ytp-settings-menu"]//span[text()="字幕"]/parent::div/parent::div/parent::div/div[@class="ytp-menuitem-content"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null)
   if(r.snapshotLength == 0 && !btn_clicked) {
-    document.querySelector('.ytp-settings-button').click()
-    btn_clicked = true
+    const settings_btn = document.querySelector('.ytp-settings-button')
+    if(settings_btn) {
+      settings_btn.click()
+      btn_clicked = true
+    }
     setTimeout(start, 100)
   } else {
     document.querySelector('.ytp-settings-button').click()
-    run(r.snapshotItem(0))
+    if(r.snapshotLength > 0) { // 否则没有字幕？
+      run(r.snapshotItem(0))
+    }
   }
 }
 
 console.log('ytcc: start')
 start()
+
+// https://www.tampermonkey.net/documentation.php#api:window.onurlchange
+if(window.onurlchange === null) {
+  // feature is supported
+  window.addEventListener('urlchange', (info) => {
+    console.log('ytcc: restart on urlchange')
+    start()
+  })
+}
 
 })()
 
