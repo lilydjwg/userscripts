1e6564c6d945be16bcddad85fc56eebf4230a8b6
lilydjwg <lilydjwg@gmail.com>
2024-04-19 19:50:17 +0800

    wikipedia-lang-reorder.user.js: fix for new theme

--- wikipedia-lang-reorder.user.js
+++ wikipedia-lang-reorder.user.js
@@ -1,69 +1,33 @@
 // ==UserScript==
-// @name           Wikipedia language reorder
-// @description	   Move Chinese and English languages to the front
+// @name           Wikipedia language tweak
+// @description	   Move Chinese and English languages to outside
 // @namespace      https://github.com/lilydjwg/userscripts
 // @match          https://*.wikipedia.org/*
 // @match          https://*.wiktionary.org/*
 // @match          https://*.wikisource.org/*
-// @version	       1.6
+// @version	   2.0
 // @grant          none
 // ==/UserScript==
 
 (function() {
 'use strict'
 
-// #p-lang for Vector legacy (2010), #p-lang-btn Vector (2022)
-const links = document.evaluate('//*[@id="p-lang" or @id="p-lang-btn"]//a/span[text()="中文" or text()="English"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null)
-let ul
-for(let i=0, len=links.snapshotLength; i<len; i++){
-  let link = links.snapshotItem(i)
-  ul = ul || link.parentNode.parentNode.parentNode
-  ul.insertBefore(link.parentNode.parentNode, ul.firstChild)
-}
+const link_en = document.querySelector('.interlanguage-link.interwiki-en')
+const link_zh = document.querySelector('.interlanguage-link.interwiki-zh')
+const bar = document.querySelector('#p-views .vector-menu-content-list') 
+may_add_it(bar, link_en)
+may_add_it(bar, link_zh)
 
-function hideTranslate() {
-  const translate = document.querySelector('.cx-new-interlanguage-link')
-  if(translate) {
-    translate.style.display = 'none'
+function may_add_it(bar, node) {
+  if(node) {
+    const new_link = node.cloneNode(true)
+    new_link.className = 'mw-list-item vector-tab-noicon'
+    if(location.href.indexOf('https://ja.') === 0 || location.href.indexOf('https://ko.') === 0) {
+      // fix different heights
+      new_link.style.fontFamily = '思源黑体'
+    }
+    bar.insertBefore(new_link, bar.childNodes[2])
   }
-}
-
-setTimeout(hideTranslate, 100)
-setTimeout(hideTranslate, 1000)
-setTimeout(hideTranslate, 3000)
-setTimeout(hideTranslate, 5000)
-
-function addStyle(css) {
-  const head = document.getElementsByTagName('head')[0]
-  if (head) {
-    const style = document.createElement('style')
-    style.setAttribute('type', 'text/css')
-    style.textContent = css
-    head.appendChild(style)
-    return style
-  }
-}
-
-// Vector (2022)
-const link_div = document.querySelector('#p-lang-btn > .vector-dropdown-content > .vector-menu-content')
-if(link_div) {
-  const link_div2 = link_div.cloneNode(true)
-  link_div2.querySelector('.after-portlet-lang').remove()
-  const alertbox = document.querySelector('.vector-language-sidebar-alert').parentElement
-  alertbox.insertAdjacentElement('afterend', link_div2)
-  alertbox.remove()
-  link_div2.id = 'my-lang-list'
-  addStyle(`
-#my-lang-list > ul {
-  list-style: none;
-  margin: 0;
-}
-#my-lang-list li {
-  padding: 0.25em 0;
-  font-size: 0.75em;
-  line-height: 1.125em;
-}
-`)
 }
 
 })()
