b02b34522810bf26b8e830b91e12f68af9dda303
lilydjwg <lilydjwg@gmail.com>
2025-02-15 11:24:09 +0800

    gitlab-tweak: try several times after page load
    the sidebar may not load immediately.

--- gitlab-tweak.user.js
+++ gitlab-tweak.user.js
@@ -10,7 +10,7 @@
 // @match          https://gitlab.gnome.org/*/merge_requests/*
 // @match          https://gitlab.freedesktop.org/*/issues/*
 // @match          https://gitlab.freedesktop.org/*/merge_requests/*
-// @version	   0.2.4
+// @version	   0.2.5
 // @grant          none
 // ==/UserScript==
 
@@ -25,23 +25,37 @@ const add_item = function(el, sidebar) {
   sidebar.insertBefore(el, sidebar.firstChild)
 }
 
-let notif, issueref
-const is_issue = location.pathname.includes('/-/issues/')
-if(is_issue) {
-  notif = document.querySelector('li[data-testid="notification-toggle"]')
-  issueref = document.querySelector('li[data-testid="copy-reference"]')
-} else {
-  notif = document.querySelector('li[data-testid="notification-toggle"]').parentNode
-}
-if(notif) {
-  const sidebar = document.querySelector('aside > div.issuable-sidebar')
+const main = function() {
+  let notif, issueref
+  const is_issue = location.pathname.includes('/-/issues/')
   if(is_issue) {
-    add_item(notif, sidebar)
-    add_item(issueref, sidebar)
-  }else{
-    sidebar.insertBefore(notif, sidebar.firstChild)
+    notif = document.querySelector('li[data-testid="notification-toggle"]')
+    issueref = document.querySelector('li[data-testid="copy-reference"]')
+  } else {
+    notif = document.querySelector('li[data-testid="notification-toggle"]').parentNode
+  }
+  if(notif) {
+    const sidebar = document.querySelector('aside > div.issuable-sidebar')
+    if(is_issue) {
+      add_item(notif, sidebar)
+      add_item(issueref, sidebar)
+    }else{
+      sidebar.insertBefore(notif, sidebar.firstChild)
+    }
+    return true
+  } else {
+    return false
   }
 }
+
+const doit = function(count) {
+  if(count <= 0) return
+  if(!main()) {
+    setTimeout(() => { doit(count-1) }, 1000)
+  }
+}
+
+doit(10)
 
 })()
 
